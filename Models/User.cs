754294fdf9b4550dfc164ae7fab26e5ca7d1c810
Maleesha Shashindi <maleeshas.2000@gmail.com>
2025-01-19 13:58:09 +0530

    Update the user model

--- Models/User.cs
+++ Models/User.cs
@@ -7,4 +7,17 @@ namespace CodeGenie.Models
         public string Email { get; set; }
         public string Password { get; set; } 
     }
+
+    public class RegisterModel
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
 }
